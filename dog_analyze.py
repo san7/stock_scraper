e2dfa836488a8c0da0ea390a9228de6dea18adc7
san7 <san7911@gmail.com>
2018-09-12 10:45:02 +0800

    fix typo

--- dog_analyze.py
+++ dog_analyze.py
@@ -60,7 +60,8 @@ selectedList = final_df[(final_df[6*width1+5+offset1] > 1) & (final_df[6*width1+
 # ============================================================
 
 # 挑近四季ROE大於20 且 外部股東報酬率大於8% 的股票
-selectedLiadd analysis fieldst = final_df[(final_df[7*width1+6+offset1] > 20) & (final_df[248] > 8)].index.tolist()
+selectedList = final_df[(final_df[7*width1+6+offset1] > 20) & (final_df[248] > 8)].index.tolist()
+
 
 # ============================================================
 
